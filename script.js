35fc8131749d4853ba9b66565db56b5368545721
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-07-24 21:22:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+
+let SubmitBotton=document.getElementById("btn")
+SubmitBotton.addEventListener('click', ()=>{
+	let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const age = Number(document.getElementById("age").value);
+        const name = document.getElementById("name").value;
+
+		if(!isNaN(age)){
+			if (age >= 18) {
+			            resolve(name);
+			        } else {
+			            reject(name);
+			        }
+				}
+        
+    }, 4000);
+});
+
+promise
+    .then((name) => alert(`Welcome, ${name}. You can vote`))
+    .catch((name) => alert(`Oh sorry ${name}. You aren't old enough`));
+
+})
